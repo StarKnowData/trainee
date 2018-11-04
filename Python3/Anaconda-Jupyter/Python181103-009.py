97a956b8813d30f8eb15a78c960e70e0dbcb78ee
liuxinshu <44717006+liuxinshu@users.noreply.github.com>
2018-11-04 10:30:47 +0800

    9组 刘心舒 09, 047

---
+++ Python3/Anaconda-Jupyter/Python181103-009.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+import time
+ 
+myD = {1: 'a', 2: 'b'}
+for key, value in dict.items(myD):
+    print(key, value)
+    time.sleep(1) # 暂停 1 秒
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
